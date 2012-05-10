21cf994380f81ef673642bf10eef29847175ee2e
jcabezas <jcabezas@localhost>
2012-05-10 07:43:33 +0200

    Added map_reduce namespace. Fixed reductions. They now should produce correct results for several configurations

--- test.cpp
+++ test.cpp
@@ -19,7 +19,7 @@
  * }}}
  */
 
-#include <cstdio>
+#include <iostream>
 #include "map"
 #include "reduce"
 
@@ -30,14 +30,174 @@
 #define C3 1.7f
 #define C4 4.9f
 
+using namespace map_reduce;
+
 int func(int x, int y)
 {
     if (x > y) return x;
     return y;
 }
 
+template <size_t N>
+int test_reduction_sum()
+{
+    array<long[N]> a;
+
+    map([&](int i)
+        {
+            a[i] = i + 1;
+        },
+        make_range(N));
+
+    long sum = reduce([&](int i)
+                      {
+                          return a[i];
+                      },
+                      [&](long a, long b)
+                      {
+                          return a + b;
+                      },
+                      make_range(N));
+
+    assert(sum == (N + 1) * N / 2);
+
+    sum = reduce([&](int i)
+                 {
+                     return a[i];
+                 },
+                 reduce_ops<int>::add,
+                 make_range(N));
+
+    assert(sum == (N + 1) * N / 2);
+
+    sum = reduce_sum([&](int i)
+                     {
+                         return a[i];
+                     },
+                     make_range(N));
+
+    assert(sum == (N + 1) * N / 2);
+
+    return 0;
+}
+
+template <size_t N, size_t M>
+int test_reduction_sum_2d()
+{
+    array<long[N][M]> a;
+    map([&](int i, int j)
+        {
+            a[i][j] = i + 1;
+        },
+        make_range(N, M));
+
+    long sum = reduce([&](int i, int j)
+                     {
+                         return a[i][j];
+                     },
+                     [&](long a, long b)
+                     {
+                         return a + b;
+                     },
+                     make_range(N, M));
+
+    assert(sum == ((N + 1) * N / 2) * M);
+
+    sum = reduce([&](int i, int j)
+                 {
+                     return a[i][j];
+                 },
+                 reduce_ops<long>::add,
+                 make_range(N, M));
+
+    assert(sum == ((N + 1) * N / 2) * M);
+
+    sum = reduce_sum([&](int i, int j)
+                     {
+                         return a[i][j];
+                     },
+                     make_range(N, M));
+
+    assert(sum == ((N + 1) * N / 2) * M);
+}
+
+template <size_t N>
+int test_reduction_max()
+{
+#define NUM 696969696
+    array<int[N]> a;
+
+    map([&](int i)
+        {
+            if (i == N/2) {
+                a[i] = int(NUM);
+            } else {
+                a[i] = i + 1;
+            }
+        },
+        make_range(N));
+
+    int max = reduce([&](int i)
+                     {
+                         return a[i];
+                     },
+                     [&](int a, int b)
+                     {
+                         return a > b? a: b;
+                     },
+                     make_range(N));
+
+    assert(max == NUM);
+
+    max = reduce([&](int i)
+                 {
+                     return a[i];
+                 },
+                 reduce_ops<int>::greater_than,
+                 make_range(N));
+
+    assert(max == NUM);
+
+    max = reduce_max([&](int i)
+                     {
+                         return a[i];
+                     },
+                     make_range(N));
+
+    assert(max == NUM);
+
+    return 0;
+}
+
+int test_reduction()
+{
+    test_reduction_sum<1>();
+    test_reduction_sum<10>();
+    test_reduction_sum<100>();
+    test_reduction_sum<1000>();
+    test_reduction_sum<10000>();
+
+    test_reduction_sum_2d<1,1>();
+    test_reduction_sum_2d<1,100>();
+    test_reduction_sum_2d<1,10000>();
+    test_reduction_sum_2d<100,1>();
+    test_reduction_sum_2d<100,100>();
+    test_reduction_sum_2d<100,10000>();
+    test_reduction_sum_2d<10000,1>();
+    test_reduction_sum_2d<10000,100>();
+    test_reduction_sum_2d<10000,10000>();
+
+    test_reduction_max<1>();
+    test_reduction_max<10>();
+    test_reduction_max<100>();
+    test_reduction_max<1000>();
+    test_reduction_max<10000>();
+}
+
 int main(int argc, char *argv[])
 {
+    test_reduction();
+
     static const unsigned N = 1000;
     static const unsigned M = 1000;
 
@@ -59,31 +219,29 @@ int main(int argc, char *argv[])
         },
         range<2>(N, M));
 
-//#define USE_SPECIALIZED
-//#define USE_LAMBDA
-
-#ifdef USE_SPECIALIZED
     int max = reduce_max([&](int i, int j)
                          {
                              return c[i][j];
                          },
                          make_range(N, M));
-#else
-    int max = reduce([&](int i, int j)
-                     {
-                         return c[i][j];
-                     },
-#ifdef USE_LAMBDA
-                     [&](int val, int tmp)
-                     {
-                         if (val > tmp) return val;
-                         return tmp;
-                     },
-#else
-                     reduce_ops<int>::greater_than,
-#endif
-                     make_range(N, M));
-#endif
+
+    max = reduce([&](int i, int j)
+                 {
+                     return c[i][j];
+                 },
+                 [&](int val, int tmp)
+                 {
+                     if (val > tmp) return val;
+                     return tmp;
+                 },
+                 make_range(N, M), reduce_sched::serial());
+
+    max = reduce([&](int i, int j)
+                 {
+                     return c[i][j];
+                 },
+                 reduce_ops<int>::greater_than,
+                 make_range(N, M), reduce_sched::serial());
 
     map([&](int i, int j)
         {
@@ -93,10 +251,19 @@ int main(int argc, char *argv[])
             }
             c[i][j] = tmp;
         },
-        make_range(N, M),
-        map_sched::parallel<1>());
+        make_range(N, M));
 
-    printf("MAX: %d\n", max);
+    map([&](int i, int j)
+        {
+            c[i][j] = reduce([&](int k)
+                             {
+                                 return a[i][k] * b[k][j];
+                             },
+                             reduce_ops<int>::add,
+                             make_range(N),
+                             reduce_sched::serial());
+        },
+        make_range(N, M));
 
     return 0;
 }
